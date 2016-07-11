736e95bb4e2ad87445d6b4bd8b5fc4e1f26d3766
Lauren Zugai <lauren@zugai.com>
2016-07-11 13:32:43 -0500

    Changed contact/portfolio links, need to update project icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(function(){
+
+  //owl carousel
+  // $('.owl-carousel').owlCarousel({
+  //   items: 4
+  // });
+
+
   $('#hide-contacts').click(function(){
     conProjView('contact','cSlideLeft', 'cSlideRight');
   });
@@ -29,18 +36,22 @@ $(function(){
     $(id).css({'-webkit-animation': animation + ' .75s', 'animation': animation + '  .75s'});
   };
 
-  projectHover('#p1', "&nbsp;(Full site)", "&nbsp; &nbsp; Magic");
-  projectHover('#p2', "&nbsp;(Full site)", "&nbsp;Dry Clean");
-  projectHover('#p3', "&nbsp; &nbsp; (Rails)", "&nbsp; &nbsp; &nbsp;HoH");
-  projectHover('#p4', "&nbsp; &nbsp; &nbsp; (JS)", "&nbsp; tictactoe");
-  projectHover('#p5', "&nbsp; (this site)", "&nbsp; &nbsp; astro");
+  //id, text displayed on hover, text displayed normally, image displayed normally
+  projectHover('#p1', '&nbsp; (Full site)<br>test', $('#p1').text(), $('#p1 img').attr('src'));
+  projectHover('#p2', '&nbsp; (Full site)', $('#p2').text());
+  projectHover('#p3', '&nbsp; &nbsp; &nbsp;(Rails)', $('#p3').text());
+  projectHover('#p4', '&nbsp; &nbsp; &nbsp; (JS)', $('#p4').text());
+  projectHover('#p5', '&nbsp; (this site)', $('#p5').text());
 
-  function projectHover(id, text1, text2) {
+  function projectHover(id, hoverText, activeText, activeImage) {
+    var hoverImage = 'img/icon-projects.png';
     $(id).hover(function() {
-      $(id+' p').html(text1);
+      $(id+' p').html(hoverText);
+      $(id+' img').attr('src', hoverImage);
     }, function() {
-      $(id+' p').html(text2);
-    })
+      $(id+' p').html(activeText);
+      $(id+' img').attr('src', activeImage);
+    });
   };
 
 });
